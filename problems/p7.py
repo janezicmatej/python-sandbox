f232a3330561f3e5e1c1eda9971066a399166ca3
Matej Janežič <janezic.mj@gmail.com>
2022-05-03 01:27:00 +0200

    problem 7

---
+++ problems/p7.py
@@ -0,0 +1,14 @@
+# Problem number 7.
+#
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the
+# 6th prime is 13.
+#
+# What is the 10 001st prime number?
+from fwk.problem import Problem
+from utils.primes import primes_to
+
+
+class Main(Problem):
+    @classmethod
+    def solution(cls) -> int:
+        return primes_to(200000)[10000]
