95f9759230f9f42f623175d4b452d782b4721bc8
Matej Janežič <janezic.mj@gmail.com>
2022-05-02 23:56:14 +0200

    problem 6

---
+++ problems/p6.py
@@ -0,0 +1,20 @@
+# Problem number 6.
+#
+# The sum of the squares of the first ten natural numbers is,
+#
+# The square of the sum of the first ten natural numbers is,
+#
+# Hence the difference between the sum of the squares of the first ten natural numbers
+# and the square of the sum is $3025 - 385 = 2640$.
+#
+# Find the difference between the sum of the squares of the first one hundred natural
+# numbers and the square of the sum.
+from fwk.problem import Problem
+
+
+class Main(Problem):
+    @classmethod
+    def solution(cls) -> int:
+        return sum([x for x in range(1, 101)]) ** 2 - sum(
+            [x**2 for x in range(1, 101)]
+        )
