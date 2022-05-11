b5d435432f7313026f291c2e52dfbd3b474196e0
Matej Janežič <janezic.mj@gmail.com>
2022-05-11 13:34:12 +0200

    problem 97

---
+++ problems/p97.py
@@ -0,0 +1,20 @@
+# Problem number 97
+#
+# The first known prime found to exceed one million digits was discovered in 1999, and
+# is a Mersenne prime of the form 26972593−1; it contains exactly 2,098,960 digits.
+# Subsequently other Mersenne primes, of the form 2p−1, have been found which contain
+# more digits.
+# However, in 2004 there was found a massive non-Mersenne prime which contains 2,357,207
+# digits: 28433×27830457+1.
+# Find the last ten digits of this prime number.
+from fwk.solution import Problem
+
+
+class Main(Problem):
+    @classmethod
+    def solution(cls) -> int:
+        count = 28433
+        for _ in range(7830457):
+            count = (count * 2) % (10**10)
+
+        return count + 1
