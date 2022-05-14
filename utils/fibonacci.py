98c25c1cac269e2a7cd2474bf0ff6140b71e95d2
Matej Janežič <janezic.mj@gmail.com>
2022-05-14 23:37:32 +0200

    problem 25

--- utils/fibonacci.py
+++ utils/fibonacci.py
@@ -1,6 +1,25 @@
 from typing import Iterator
+import sys
+from typing import Any, Callable, TypeVar, cast, ParamSpec
 
 
+F = TypeVar("F", bound=Callable[..., Any])
+P = ParamSpec("P")
+
+
+def recursion_extender(depth: int = 1000) -> Callable[[F], Callable]:
+    sys.setrecursionlimit(depth)
+
+    def wrap(fun: F) -> Callable[[F], Callable]:
+        def inner(*args: P.args, **kwargs: P.kwargs) -> Callable:
+            return fun(*args, **kwargs)
+
+        return cast(F, inner)
+
+    return wrap
+
+
+@recursion_extender(100000)
 def fib_generator(previous: int = 1, current: int = 0) -> Iterator[int]:
     yield previous + current
     for next_fib in fib_generator(current, previous + current):
